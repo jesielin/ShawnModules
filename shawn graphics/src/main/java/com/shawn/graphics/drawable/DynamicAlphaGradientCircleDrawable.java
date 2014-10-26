81596b66291312db01ecb0e3ebe42652fd157a41
Shawn <fengxiaojun1990@qq.com>
2014-10-26 13:49:57 +0800

    change graphics module name

---
+++ shawn graphics/src/main/java/com/shawn/graphics/drawable/DynamicAlphaGradientCircleDrawable.java
@@ -0,0 +1,14 @@
+package com.shawn.graphics.drawable;
+
+import android.content.Context;
+
+/**
+ * Created by shawn on 14/10/24.
+ */
+public class DynamicAlphaGradientCircleDrawable extends AlphaCircleProgressDrawable {
+
+
+    public DynamicAlphaGradientCircleDrawable(Context context, int style, float progress) {
+        super(context, style, progress);
+    }
+}
