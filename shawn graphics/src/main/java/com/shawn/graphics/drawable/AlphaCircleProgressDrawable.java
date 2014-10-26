4d8bb3dd7795c1f953773c065cab7f4a0eab0889
Shawn <fengxiaojun1990@qq.com>
2014-10-26 13:53:50 +0800

    why cannot sync remote

--- shawn graphics/src/main/java/com/shawn/graphics/drawable/AlphaCircleProgressDrawable.java
+++ shawn graphics/src/main/java/com/shawn/graphics/drawable/AlphaCircleProgressDrawable.java
@@ -4,16 +4,11 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
-import android.util.Log;
-import android.view.animation.AlphaAnimation;
-
-import com.mustafaferhan.debuglog.DebugLog;
 import com.shawn.R;
 
 /**
